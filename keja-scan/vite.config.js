099c55c078499bdb7a4c2bc14735448cfc3b28bb
keestra-ke <kevinmasinde796@gmail.com>
2026-05-15 13:55:10 +0300

    Update Vite config to set base path
    Removed server configuration and set base path.

--- keja-scan/vite.config.js
+++ keja-scan/vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Replace 'keja-scan' with your actual GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173
-  }
+  base: '/keja-scan/', 
 })
